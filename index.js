ebeb5cff80d629d057868e768b21fe514b0680c9
David Sttivend <dsangel@gmail.com>
2019-09-11 20:00:38 -0500

    Added delete event

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var toDoForm = document.getElementById("toDoForm")
-
+var ulTask=document.getElementById("ulTask")
 toDoForm.addEventListener("submit", task)
+var deleteButton=document.getElementById("deleteButton")
+deleteButton.addEventListener("click",deleteTask)
 
 function task(event){
     event.preventDefault()
-    console.log(event.target[0])
-    console.log(event.target[1])
+    if (event.target[0].value!==""){
+    var liTask=document.createElement("li")
+    var label=document.createElement("label")
+    var inputCheck=document.createElement("input")
+    inputCheck.type="checkbox"
+    ulTask.appendChild(liTask)
+    label.innerHTML=event.target[0].value
+    liTask.appendChild(label)
+    label.appendChild(inputCheck)
+    toDoForm.reset()}
+}
+
+function deleteTask(){
+    Array.from(ulTask.children).forEach(function(li){
+        if (li.children[0].children[0].checked){
+            ulTask.removeChild(li)   
+        }
+    }); 
 }
